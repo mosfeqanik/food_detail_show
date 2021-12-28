4172caa8873563085990e87ce2e4c5344353dffa
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-28 23:04:46 +0600

    done with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:food_view_provider/providers/foods_provider.dart';
+import 'package:food_view_provider/screens/food_detail_page.dart';
+import 'package:food_view_provider/screens/food_overview_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return ChangeNotifierProvider<Foods>(
+      create: (context)=> Foods(),
+      child: MaterialApp(
+          title: 'Food Order',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            accentColor: Colors.orange,
+            fontFamily: 'Lato',
+          ),
+          home: FoodsOverviewScreen(),
+          routes: {
+            FoodDetailScreen.routeName: (ctx) => FoodDetailScreen(),
+          }
       ),
     );
+
   }
 }
